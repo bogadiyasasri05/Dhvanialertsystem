caebd1b4e4a6a35eb86d2605e8a34be6bcdbfafc
bogadiyasasri05 <bogadiyasasri@gmail.com>
2026-03-31 22:32:22 +0530

    Update model_loader.py

--- app/model_loader.py
+++ app/model_loader.py
@@ -2,14 +2,14 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras.models import Model
 
-MODEL_PATH = "models/dhvani_model.keras"
-PROTO_PATH = "models/class_prototypes.npy"
+MODEL_PATH = "app/models/dhvani_model.keras"
+PROTO_PATH = "app/models/class_prototypes.npy"
 
-# Load main model
+# Load model
 base_model = tf.keras.models.load_model(MODEL_PATH, compile=False)
 print("✅ Model loaded")
 
-# Create feature extractor
+# Feature extractor
 feature_model = Model(
     inputs=base_model.input,
     outputs=base_model.get_layer("feature_extractor").output
@@ -20,12 +20,10 @@ print("✅ Feature extractor ready")
 prototypes = np.load(PROTO_PATH, allow_pickle=True).item()
 print("✅ Prototypes loaded")
 
-# Class labels
 class_names = [
     'air_conditioner', 'car_horn', 'children_playing', 'dog_bark',
     'drilling', 'engine_idling', 'gun_shot',
     'jackhammer', 'siren', 'street_music'
 ]
 
-# Threshold
 OSR_THRESHOLD = 8.5
