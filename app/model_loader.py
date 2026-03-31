cac087e4c5ac0a8e6fa404de572787581380653e
bogadiyasasri05 <bogadiyasasri@gmail.com>
2026-04-01 03:14:29 +0530

    Update model_loader.py

--- app/model_loader.py
+++ app/model_loader.py
@@ -4,17 +4,25 @@ import numpy as np
 MODEL_PATH = "app/models/dhvani_model_clean.h5"
 PROTO_PATH = "app/models/class_prototypes.npy"
 
-# ✅ Load FULL trained model (NO manual architecture)
-feature_model = tf.keras.models.load_model(MODEL_PATH, compile=False)
-
-# ✅ Load prototypes
-prototypes = np.load(PROTO_PATH, allow_pickle=True)
-
-# ✅ Extract class names
-if isinstance(prototypes, dict):
-    class_names = list(prototypes.keys())
-    prototypes = np.array(list(prototypes.values()))
-else:
-    class_names = [f"class_{i}" for i in range(len(prototypes))]
-
+feature_model = None
+prototypes = None
+class_names = None
 OSR_THRESHOLD = 0.5
+
+def load_model_once():
+    global feature_model, prototypes, class_names
+
+    if feature_model is None:
+        print("🔄 Loading model...")
+
+        feature_model = tf.keras.models.load_model(MODEL_PATH, compile=False)
+
+        prototypes = np.load(PROTO_PATH, allow_pickle=True)
+
+        if isinstance(prototypes, dict):
+            class_names = list(prototypes.keys())
+            prototypes = np.array(list(prototypes.values()))
+        else:
+            class_names = [f"class_{i}" for i in range(len(prototypes))]
+
+        print("✅ Model loaded successfully")
